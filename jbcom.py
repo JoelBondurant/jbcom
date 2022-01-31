a13e94efb422305dd5cd557ea1311244e31f4f66
Joel Bondurant <joelbondurant@protonmail.com>
2022-01-31 06:19:55 +0000

    Make routes async.

--- jbcom.py
+++ jbcom.py
@@ -14,18 +14,18 @@ app.mount('/photos', StaticFiles(directory='photos'), name='photos')
 templates = Jinja2Templates(directory='html')
 
 @app.get('/', response_class=HTMLResponse)
-def get_index(req: Request):
+async def get_index(req: Request):
 	return templates.TemplateResponse('index.html', {'request': req})
 
 @app.get('/resume', response_class=HTMLResponse)
-def get_resume(req: Request):
+async def get_resume(req: Request):
 	return templates.TemplateResponse('resume.html', {'request': req})
 
 @app.get('/photos', response_class=HTMLResponse)
-def get_photos(req: Request):
+async def get_photos(req: Request):
 	return templates.TemplateResponse('photos.html', {'request': req})
 
 @app.get('/calendly', response_class=HTMLResponse)
-def get_calendly(req: Request):
+async def get_calendly(req: Request):
 	return templates.TemplateResponse('calendly.html', {'request': req})
 
