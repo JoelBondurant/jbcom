bd09d7fdc0f5066ffe32983b552149670661686c
Joel Bondurant <joelbondurant@pm.me>
2022-02-04 02:10:02 -0600

    Hit counter added.

--- jbcom.py
+++ jbcom.py
@@ -1,12 +1,15 @@
-from typing import Optional
-
+import aioredis
+import orjson
 from fastapi import FastAPI, Request, Header
 from fastapi.responses import HTMLResponse, ORJSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 
+app_context = {}
+
 app = FastAPI()
+app_context['app'] = app
 
 app.mount('/css', StaticFiles(directory='css'), name='css')
 app.mount('/js', StaticFiles(directory='js'), name='js')
@@ -15,9 +18,26 @@ app.mount('/photos', StaticFiles(directory='photos'), name='photos')
 
 templates = Jinja2Templates(directory='html')
 
+
+@app.on_event('startup')
+async def on_startup():
+	with open('/secrets/jbcom.json', 'r') as fin:
+		config = orjson.loads(fin.read())
+	app_context['config'] = config
+
+	redis = await aioredis.create_redis_pool(
+		'redis://' + config['redis']['host'],
+		password=config['redis']['password'])
+	app_context['redis'] = redis
+
+
 @app.get('/', response_class=HTMLResponse)
 async def get_index(req: Request):
-	return templates.TemplateResponse('index.html', {'request': req})
+	hit_counter = await app_context['redis'].incr('jbcom_hit_counter')
+	return templates.TemplateResponse('index.html', {
+		'request': req,
+		'hit_counter': hit_counter
+	})
 
 @app.get('/resume', response_class=HTMLResponse)
 async def get_resume(req: Request):
@@ -37,7 +57,9 @@ async def get_calendly(req: Request):
 
 @app.get('/trace', response_class=ORJSONResponse)
 async def get_trace(req: Request):
-	return {
+	resp = {
 		'X-Forwarded-For': req.headers.get('X-Forwarded-For'),
 		'User-Agent': req.headers.get('User-Agent'),
 	}
+	return resp
+
