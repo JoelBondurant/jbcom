2dcd1cb00a849cac1fe2819ab3db15d2efea7671
Joel Bondurant <joelbondurant@pm.me>
2022-02-19 08:53:45 -0600

    Add static telephone path.

--- jbcom.py
+++ jbcom.py
@@ -11,6 +11,8 @@ app_context = {}
 app = FastAPI()
 app_context['app'] = app
 
+tn = '7752256586'
+app.mount(f'/{tn}', StaticFiles(directory=tn), name=tn)
 app.mount('/css', StaticFiles(directory='css'), name='css')
 app.mount('/js', StaticFiles(directory='js'), name='js')
 app.mount('/img', StaticFiles(directory='img'), name='img')
