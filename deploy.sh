59bcac43608f3c0ab3dc518e56bff3e1e04eb474
Joel Bondurant <em@joelbondurant.com>
2026-02-17 11:45:25 -0600

    Binding to port 443 from non-root user isn't sticky through setcap, upgrade to a system unit.

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,17 @@
 printf "jbcom deployment started...  %s\n" "$(date -u -Iseconds)"
 ssh me@jbcom "
-	mkdir -p /home/me/.config/systemd/user
 	mkdir -p /home/me/photos
 	mkdir -p /home/me/static
 "
 scp -r static/ me@jbcom:/home/me/
 scp target/release/jbcom me@jbcom:/home/me/jbcom.deploy
-scp jbcom.service me@jbcom:/home/me/.config/systemd/user/jbcom.service
+scp jbcom.service me@jbcom:/home/me/jbcom.service
 ssh me@jbcom "
-	systemctl --user daemon-reload
-	systemctl --user stop jbcom
+	sudo mv -f /home/me/jbcom.service /etc/systemd/system/jbcom.service
+	sudo systemctl daemon-reload
+	sudo systemctl stop jbcom
 	mv -f /home/me/jbcom.deploy /home/me/jbcom
-	sudo setcap 'cap_net_bind_service=+ep' /home/me/jbcom
-	systemctl --user enable jbcom
-	systemctl --user start jbcom
+	sudo systemctl enable jbcom
+	sudo systemctl start jbcom
 "
 printf "jbcom deployment finished.  %s\n" "$(date -u -Iseconds)"
