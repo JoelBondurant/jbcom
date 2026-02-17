0b92a24b1e2ca9037785cc6fd85673a19cccc22a
Joel Bondurant <em@joelbondurant.com>
2026-02-17 16:15:05 -0600

    Security hardening, move to dedicated path /opt/jbcom.

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,19 @@
 printf "jbcom deployment started...  %s\n" "$(date -u -Iseconds)"
 ssh me@jbcom "
-	mkdir -p /home/me/photos
-	mkdir -p /home/me/static
+	sudo mkdir -p /opt/jbcom/photos
+	sudo mkdir -p /opt/jbcom/static
+	sudo chown -R me:me /opt/jbcom
 "
 scp -r static/ me@jbcom:/home/me/
-scp target/release/jbcom me@jbcom:/home/me/jbcom.deploy
 scp jbcom.service me@jbcom:/home/me/jbcom.service
+scp target/release/jbcom me@jbcom:/home/me/jbcom
 ssh me@jbcom "
 	sudo mv -f /home/me/jbcom.service /etc/systemd/system/jbcom.service
 	sudo systemctl daemon-reload
 	sudo systemctl stop jbcom
-	mv -f /home/me/jbcom.deploy /home/me/jbcom
+	mv -f /home/me/jbcom /opt/jbcom/jbcom
+	rm -rf /opt/jbcom/static
+	mv -f /home/me/static /opt/jbcom/
 	sudo systemctl enable jbcom
 	sudo systemctl start jbcom
 "
