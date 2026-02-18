85891de16c9118d59f1a7afe03879e810380e444
Joel Bondurant <em@joelbondurant.com>
2026-02-17 22:11:07 -0600

    Bump vanta/threejs versions, remove old port binding scripts.

--- bind.sh
+++
@@ -1,3 +0,0 @@
-printf "binding to port 443 started...  %s\n" "$(date -u -Iseconds)"
-sudo setcap 'cap_net_bind_service=+ep' target/release/jbcom
-printf "binding to port 443 finished.  %s\n" "$(date -u -Iseconds)"
--- run.sh
+++
@@ -1,5 +0,0 @@
-printf "jbcom started...  %s\n" "$(date -u -Iseconds)"
-for i in {1..3}; do ./bind.sh && break || sleep 2; done
-#cargo watch -s "./bind.sh" -x run
-sleep 2
-./target/release/jbcom
